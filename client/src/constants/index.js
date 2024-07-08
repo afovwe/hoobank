b9e4e9f6ece82af203163d0d0fba988f95c44cf4
Omofovwe Akpadaka <afovwe@gmail.com>
2024-07-08 08:01:46 -0700

    social media links updated

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -158,22 +158,22 @@ export const socialMedia = [
   {
     id: "social-media-1",
     icon: instagram,
-    link: "https://www.instagram.com/",
+    link: "https://www.instagram.com/afovwe/",
   },
   {
     id: "social-media-2",
     icon: facebook,
-    link: "https://www.facebook.com/",
+    link: "https://web.facebook.com/akpadaka.fovwe",
   },
   {
     id: "social-media-3",
     icon: twitter,
-    link: "https://www.twitter.com/",
+    link: "https://x.com/oakpadaka",
   },
   {
     id: "social-media-4",
     icon: linkedin,
-    link: "https://www.linkedin.com/",
+    link: "https://www.linkedin.com/in/peter-odjenima/",
   },
 ];
 
